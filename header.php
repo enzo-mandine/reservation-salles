09891463dfa9ea42ef6ed7e9089976b8d94b9487
Samuel <Samuel.joly@laplateforme.io>
2019-12-10 23:48:13 +0100

    Styling modification (not important)

--- header.php
+++ header.php
@@ -10,20 +10,29 @@
 
 <header>
 	<nav class="flexr">
-		<ul class="ul_style_none flexr mp0">
+		<ul class="ul_style_none flexr ">
 			<li class=""> <a href="">Accueil</a></li>
 			<li class=""> <a href="">Reservation</a></li>
 			<li><a href=""><!--INSERT RESERVATION-FORM.PHP HERE--></a></li>
 			
-			<li class="">
 <?php 			if (!isset($_SESSION["isconnected"])) { ?>
-					<a class='' href='inscription.php'>Inscription</a>
-					<a class='' href='connexion.php'>Connexion</a>
+					<li class="">
+						<a class='' href='inscription.php'>Inscription</a>
+					</li>
+					
+					<li>
+						<a class='' href='connexion.php'>Connexion</a>
+					</li>
 <?php			} else { ?>
-					<a class='' href='index.php?logout=true'>Deconnexion</a>
-					<a class='' href='profil.php'>Mon compte</a>
+					<li>
+						<a class='' href='index.php?logout=true'>Deconnexion</a>
+					</li>
+					
+					<li>
+						<a class='' href='profil.php'>Mon compte</a>
+					</li>
 <?php			}?>
-			</li>
+			
 			
 		</ul>
 	</nav>
--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link rel="stylesheet" href="style.css">
+		<title>Accueil</title>
+	</head>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <title>Accueil</title>
-</head>
+	<body class="mp0">
+		<?php include("header.php"); ?>
+		
+		<main>
 
-<body class="mp0">
-    <?php include("header.php"); ?>
-    <main>
-
-    </main>
-    <footer>
-        <div id="" class="">
-            <p class="">Réservations de salles - Laplateforme</p>
-        </div>
-    </footer>
-</body>
-	
+		</main>
+		
+		<footer>
+			<div id="" class="">
+				<p class="">Réservations de salles - Laplateforme</p>
+			</div>
+		</footer>
+	</body>	
 </html>
