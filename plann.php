dfb215189c994454a3a754d829861c12d55654af
Samuel <Samuel.joly@laplateforme.io>
2019-12-17 09:54:09 +0100

    Added where to sql query for limiting searching

--- plann.php
+++ plann.php
@@ -12,8 +12,9 @@
 	$thisWeek = intval( date("j", strtotime($year."W".floor(( $day_of_year/7)+1))) );
 	$days = array("Lundi","Mardi","Mercredi","Jeudi","Vendredi");
 	
-	$request_reservations = "SELECT titre, utilisateurs.login, date_format(debut,'%w %k'), date_format(fin,'%w %k') 
-							FROM reservations INNER JOIN utilisateurs ON reservations.id_utilisateur = utilisateurs.id";
+	$request_reservations = "SELECT titre, utilisateurs.login, date_format(debut,'%w %k %d'), date_format(fin,'%w %k %d') 
+							FROM reservations INNER JOIN utilisateurs ON reservations.id_utilisateur = utilisateurs.id
+							WHERE date_format(debut, '%d') >= '".$thisWeek."'";
 	$reservations = sql_request($request_reservations, true);
 	
 	$hour = 7;					
