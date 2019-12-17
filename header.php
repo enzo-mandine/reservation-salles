b696448c935f41f1a6d3c15bc1c5704b89e8acae
Samuel <Samuel.joly@laplateforme.io>
2019-12-17 23:36:43 +0100

    Debug some shit about the header php, <?php followed by } made an error in the code

--- header.php
+++ header.php
@@ -1,25 +1,29 @@
 <?php
-session_start();
-include("function.php");
+	include("function.php");
+	
+	session_start();
 
-if (isset($_GET["logout"])) {
-	session_destroy();
-	header("location:index.php");
-}
-
-if (isset($_SESSION["block"])) {
-	if (time() - $_SESSION["block"] > 60) {
-		unset($_SESSION["block"]);
+	if (isset($_GET["logout"])) 
+	{
+		session_destroy();
 		header("location:index.php");
-	}	?>
+	}
 
-	<div id="greyScreen">
-		<p id="err">Vous etes bloqué pour 60 secondes</p>
-	</div>
+	if (isset($_SESSION["block"])) 
+	{
+		if (time() - $_SESSION["block"] > 60) 
+		{
+			unset($_SESSION["block"]);
+			header("location:index.php");
+		}	?>
+
+		<div id="greyScreen">
+			<p id="err">Vous etes bloqué pour 60 secondes</p>
+		</div>
 
 <?php
-}	?>
-<link rel="stylesheet" href="style.css">
+	}	?>
+	
 <header>
 	<nav id="nav_header" class="flexr">
 		<ul id="nav_ul" class="ul_style_none flexr">
@@ -29,21 +33,25 @@ if (isset($_SESSION["block"])) {
 			<li class="">
 				<a href="planning.php">Reservation</a>
 			</li>
-			<?php if (!isset($_SESSION["isconnected"])) { ?>
-				<li class="">
-					<a class='' href='inscription.php'>Inscription</a>
-				</li>
-				<li>
-					<a class='' href='connexion.php'>Connexion</a>
-				</li>
-			<?php			} else { ?>
-				<li>
-					<a class='' href='index.php?logout=true'>Deconnexion</a>
-				</li>
-				<li>
-					<a class='' href='profil.php'>Mon compte</a>
-				</li>
-			<?php			} ?>
+			<?php 
+				if (isset($_SESSION["isconnected"])) 
+				{ ?>
+					<li>
+						<a class='' href='index.php?logout=true'>Deconnexion</a>
+					</li>
+					<li>
+						<a class='' href='profil.php'>Mon compte</a>
+					</li>
+		<?php 	} 
+				else 
+				{ ?>
+					<li class="">
+						<a class='' href='inscription.php'>Inscription</a>
+					</li>
+					<li>
+						<a class='' href='connexion.php'>Connexion</a>
+					</li>
+		<?php 	} ?>
 		</ul>
 	</nav>
 </header>
