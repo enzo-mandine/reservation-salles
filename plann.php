ffc2314073e7e72eb44ab137c26ea26d0a673d6f
Samuel <Samuel.joly@laplateforme.io>
2019-12-25 18:35:20 +0100

    Added nex week option and readjust max month size for numeric date sdisplaying

--- plann.php
+++ plann.php
@@ -5,12 +5,21 @@
 	$year = getdate()["year"];
 	$day_of_year = getdate()["yday"];
 	
-	$thisWeek = intval( date("j", strtotime($year."W".floor(( $day_of_year/7)+1))) );
+	
+	// 31 30 31 30 31 30 31 31 30 31 30 31
+	if(isset($_GET["page"]))
+	{
+		$day_of_year += 7*$_GET["page"];
+	}
+	
+	$max_month_date = intval( date("t", strtotime($year."W".floor(($day_of_year/7)+1))));
+	$cur_month = 1+intval( date("m", strtotime($year."W".floor(($day_of_year/7)+1))));
+	$thisWeek = intval( date("j", strtotime($year."W".floor(($day_of_year/7)+1))));
 	$days = array("Lundi","Mardi","Mercredi","Jeudi","Vendredi");
 	
 	$request_reservations = "SELECT titre, utilisateurs.login, date_format(debut,'%w %k %d'), date_format(fin,'%w %k %d') 
 							FROM reservations INNER JOIN utilisateurs ON reservations.id_createur = utilisateurs.id
-							WHERE date_format(debut, '%d') >= '".$thisWeek."'";
+							WHERE date_format(debut, '%d %c %Y') >= '".$thisWeek." ".$cur_month." ".$year."'";
 	$reservations = sql_request($request_reservations, true);
 	
 	$hour = 7;					
@@ -32,6 +41,10 @@
 			if($hour == 7 && $day > 0)
 			{
 				$date = $thisWeek + ($day-1);
+				if ($date > $max_month_date)
+				{
+					$date -= $max_month_date;
+				}
 				echo "<th>".$days[$day-1]."-".$date."</th>";
 			}
 			else if($day == 0 && $hour > 7)
@@ -76,6 +89,24 @@
 			echo "</tr>";			
 		}
 		$hour++;
+	}
+	
+	
+	if(!isset($_GET["page"]))
+	{?>
+		<a href="planning.php?page=1">Semaine suivante</a>
+<?php
+	}
+	else
+	{  if($_GET["page"] >= 1) {?>
+		<a href="planning.php?page=<?php echo $_GET["page"]+1; ?>">Semaine suivante</a><br/>
+		<a href="planning.php?page=<?php echo $_GET["page"]-1; ?>">Semaine precedente</a>
+<?php	
+		}
+		else
+		{?>
+			<a href="planning.php?page=<?php echo $_GET["page"]+1; ?>">Semaine suivante</a><br/>
+<?php	}
 	}
 ?>
 
