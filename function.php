f2d72ca8d46029413a471f3a5227c9d6436d4009
[enzo.mandine] <[enzo.mandine@laplateforme.io]>
2020-01-06 10:30:02 +0100

    Merge function.php

--- function.php
+++ function.php
@@ -15,7 +15,55 @@
 				return mysqli_fetch_all($query);				
 			}
 		}
-		
 		mysqli_close($conn);
+	}	
+	
+	function required($form)
+	{
+		foreach($form as $input)
+		{
+			if(empty($input))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	function error($str)
+	{ ?>
+		<div class="err">
+			<p><?php echo $str; ?><a href="index.php"><img src="Images/closeBtn.png"/></a></p>
+		</div>
+<?php 
+	}  
+	
+	
+	function check_reservation($reservations, $day, $hour, $curTime)
+	{
+		echo "<td>";
+
+		$is_reserved = false; // Vérificateur de réservation (voir ligne 62)
+		
+		foreach($reservations as $reservation)	{ // On boucle dans les réservations sélectionnées
+			$reservation_day  =	explode(" ",$reservation[2])[0]; // date_format(debut,"%w") -> le jour de la réservation
+			$reservation_hour = explode(" ",$reservation[2])[1]; // date_format(debut,"%c")  -> l'heure de la réservation
+			$reservation_date = explode(" ",$reservation[2])[2]; // date_format(debut,"%Y")  -> la date de la réservation
+			
+			// Si le jour de la réservation correspond a la valeur de $day, que l'heure correspond a la valeur de $hour et que la
+			// date correspond a la date relative a $curTime
+			if($reservation_day == $day && $reservation_hour == $hour && $reservation_date == date("d", strtotime("+".($day-1)." day",$curTime)))	{?>
+				<a href='reservation.php?id=<?php echo $reservation[3]; ?>'><?php echo $reservation[0]; ?></a> 
+		<?php	$is_reserved = true; // On affiche le titre de la réservation dans un lien qui redirige vers la page de reservation
+									 // avec l'id de la reservation en get et on précise que la case est reservée avec $is_reserved
+			}						
+		}
+		
+		if(!$is_reserved)	{ // Si la case n'a pas de réservation associé on affiche un bouton pour prendre une réservation
+			echo "<a href='reservation-form.php'><input class='btn_add' type='button' value='+'></a>";
+		}
+
+		echo "</td>";
+		
 	}
 ?>
