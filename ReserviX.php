bd65a9674e9ac22e2b878e7408c5606de4239781
Samuel <Samuel.joly@laplateforme.io>
2020-01-06 23:47:47 +0100

    reservix basic

--- ReserviX.php
+++ ReserviX.php
@@ -1,42 +1,72 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="style.css">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-	<title>Accueil</title>
-</head>
-
 <body class="mp0">
 
-	<?php include("header.php"); ?>
-	<main>
-		<article id="intro">
-			<p>Bienvenu sur le mode RESERVIX 2000 des salles vidéo/audio de La Plateforme_ !<br />Vous avez un projet ?</p>
-		</article>
-		<a href=<?php if (isset($_SESSION["isconnected"])) {
-					echo "planning.php";
-				} else {
-					echo "connexion.php";
-				} ?>>
-			<article id="box" class="flexc gradient-border">
-				<p class="txt_center font15pt">Réservez sur la durée j'usqu'a 2h/jour selon vos disponibilités ! </p>
-			</article>
-		</a>
+
+		<article class="flexcol">
 		
-		<article class="flexcol marg-pad mid-width-height">
-		
-			<form class="flexrow flex_center marg-pad mid-width-height" method="post" action="">
-				<input type="checkbox" class="" value="Monday" name="jour"/>
-			
+			<form class="reservix-form" method="post" action="">
+				
+				
+				<div id="day-zone" class="input-zone">
+					<label for="jour">Quels jours :</label>
+					<input type="checkbox" class="" value="Monday" name="jour">Lundi
+					<input type="checkbox" class="" value="Tuesday" name="jour">Mardi
+					<input type="checkbox" class="" value="Wednesday" name="jour">Mercredi
+					<input type="checkbox" class="" value="Thursday" name="jour">Jeudi
+					<input type="checkbox" class="" value="Friday" name="jour">Vendredi
+				</div>
+				
+				<div class="input-zone">
+					<label for="hour">Quelle heure ?</label>
+					<input type="time" name="hour" min="08:00" max="17:00" value="08:00"/>
+				</div>
+				
+				<div class="input-zone">
+					<label for="date">A partir</label>
+					<input type="datetime-local" name="date"/>
+				</div>
+				
+				<div class="input-zone">
+					<label for="repeat">Pendant</label>
+					<select name="repeat">
+						<option value="1">1</option>
+						<option value="2">2</option>
+						<option value="3">3</option>
+						<option value="4">4</option>
+						<option value="5">5</option>
+						<option value="6">6</option>
+						<option value="7">7</option>
+						<option value="8">8</option>
+						<option value="9">9</option>
+						<option value="10">10</option>
+						<option value="11">11</option>
+						<option value="12">12</option>
+					</select>
+					<select name="unit">
+						<option value="sem">Semaines</option>
+						<option value="mois">Mois</option>
+					</select>
+				</div>
+				
+				<div class="input-zone">
+					<label for="titre">Titre</label>
+					<input type="text" name="titre"/>
+				</div>
+				
+				<div class="input-zone" id="textarea">
+					<label for="desc">Description</label>
+					<textarea name="desc" cols="50" rows="5"></textarea>
+				</div>
+				
+				
+				
+				<input type="submit" name="submitBtn"/>
 			</form>
 		
 		</article>
 	</main>
-	<?php include("footer.php"); ?>
 </body>
 
 </html>
@@ -49,24 +79,22 @@
 		flex-direction:column;
 	}
 	
-	.flexrow{
+	.reservix-form{
 		display:flex;
-		flex-direction:row;
+		flex-direction:column;
+		margin:auto;
+		border-radius:5px;
+		background:#ffffff;
 	}
 	
-	.flex_center {
-		align-self:center;
-	}
-	
-	.marg-pad{
-		margin:15px;
-		padding:15px;
-	}
-	
-	.mid-width-height
-	{
-		width:50vh;
-		height:50vh;
+	.input-zone {
+		margin:10px;
+		align-self:flex-start;
 	}
 
+	#textarea
+	{
+		display:flex;
+		flex-direction:column;
+	}
 </style>
