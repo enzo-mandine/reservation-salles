fa96f48bb3b607042d1c7054681e8f853fbbef8c
[enzo.mandine] <[enzo.mandine@laplateforme.io]>
2020-01-08 18:49:50 +0100

    PUSH DB

--- reservationsalles.sql
+++ reservationsalles.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 07 jan. 2020 à 13:02
+-- Généré le :  mer. 08 jan. 2020 à 17:49
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `reservations` (
   `fin` datetime NOT NULL,
   `id_utilisateur` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `reservations`
@@ -50,7 +50,8 @@ INSERT INTO `reservations` (`id`, `titre`, `description`, `debut`, `fin`, `id_ut
 (2, 'test res 2', 'res 2', '2020-01-08 08:00:00', '2020-01-08 09:00:00', 1),
 (3, 'test res 3', 'res 3', '2020-01-09 08:00:00', '2020-01-09 09:00:00', 1),
 (5, 'zerfghjkl', 'gfhg', '2020-01-10 11:00:00', '2020-01-10 12:00:00', 2),
-(6, 'kkkk', 'gfdtfhgvj', '2020-01-07 10:48:00', '2020-01-07 11:00:00', 2);
+(6, 'kkkk', 'gfdtfhgvj', '2020-01-07 10:48:00', '2020-01-07 11:00:00', 2),
+(9, 'caca', 'caca', '2020-01-10 08:00:00', '2020-01-10 09:00:00', 10);
 
 -- --------------------------------------------------------
 
@@ -63,18 +64,21 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `avatar` varchar(255) NOT NULL COMMENT 'name = id.png',
+  `image` varchar(255) NOT NULL COMMENT 'name = id.png',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `utilisateurs`
 --
 
-INSERT INTO `utilisateurs` (`id`, `login`, `password`, `avatar`) VALUES
+INSERT INTO `utilisateurs` (`id`, `login`, `password`, `image`) VALUES
 (1, 'admin', 'admin', ''),
 (2, 'Enzo', '$2y$10$T7RHDI3nytLklPANiNe8D.R/VaraMOr.mIKW8HJzJCDoM4TOAbvku', ''),
-(5, 'eti', '$2y$10$RMWyoU9JDo.5ybp/vTUpGusDYuWsrL5G1H4l0cFx0Ixy7nfMefgtC', '');
+(5, 'eti', '$2y$10$RMWyoU9JDo.5ybp/vTUpGusDYuWsrL5G1H4l0cFx0Ixy7nfMefgtC', ''),
+(6, 'avatar', '$2y$10$hYrOgEpGTffbhEYp5qONgOFhXFN7P.BJpOznicGhjzEsnFwTVmACa', ''),
+(7, 'marceau', '$2y$10$HHxX/rS5nMLqiFDPHMxbOOpU0Rn.GevQkcQhxG4jxsoCRDOIMUK9e', '0.png'),
+(10, 'caca', '$2y$10$V.Ob0XQ4ojuUt5Qe2H.MXOujdqxT2Og.sJJDIgjOislcOGF9mj5ca', 'ProfilPics/10.png');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
