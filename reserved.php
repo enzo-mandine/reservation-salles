a2791248f50e8bfec45f2e2b0ce2f8d64582e1d1
Samuel <Samuel.joly@laplateforme.io>
2020-01-08 07:34:31 +0100

    Added modification and suppression button on reserved page

--- reserved.php
+++ reserved.php
@@ -13,7 +13,9 @@
     <?php include("header.php"); ?>
     <?php
     //$request = "SELECT * FROM reservations WHERE id = '" . $_GET["id"] . "'";
-    $request = "SELECT * FROM `reservations` INNER JOIN `utilisateurs` ON utilisateurs.id = reservations.id_utilisateur WHERE reservations.id = '" . $_GET["id"] . "'";
+    $request = "SELECT * FROM `reservations`
+				INNER JOIN `utilisateurs` ON utilisateurs.id = reservations.id_utilisateur 
+				WHERE reservations.id = '" . $_GET["id"] . "'";
     $resultat = sql_request($request, true, true);
     ?>
     <main>
@@ -26,11 +28,64 @@
                     <p class="txt_center max_width font15pt">Début: <?php echo $resultat[3] ?> / Fin: <?php echo $resultat[4] ?></p>
                     <p class="txt_center max_width font15pt">Description:</p>
                     <p class="txt_center max_width font15pt"><?php echo $resultat[2] ?></p>
+					
+					<?php
+						if(isset($_SESSION["id"])) {
+							if($_SESSION["id"] == $resultat[5]) {
+								?>
+									<div style="display:flex;">
+										<a href="reserved.php?id=<?php echo $resultat[0]; ?>&&deleted=true"><input type="button" class="res_slot" value="Supprimer"/></a>
+										<a href="reserved.php?id=<?php echo $resultat[0]; ?>&&modified=true"><input type="button" class="res_slot" value="Modifier"/></a>
+									</div>
+								<?php
+							}
+						}
+					?>
                 </div>
             </div>
         </article>
     </main>
+	
+	<?php
+	
+		if(isset($_GET["deleted"])) {
+			if($_SESSION["id"] == $resultat[5]) {
+				sql_request("DELETE FROM reservations WHERE reservations.id = '".$resultat[0]."' ");
+				error("L'évenement ".$resultat[1]." est supprimé","planning.php");
+			}
+		}
+		
+		if(isset($_GET["modified"])) {
+			if($_SESSION["id"] == $resultat[5]) {	?>
+					<div id="greyScreen">
+						<form action="" method="POST" id="err">
+							<label for="titre">Titre de l'évenement</label>
+							<input type="text" name="titre" value="<?php echo $resultat[1]; ?>" required>
+							<label for="desc">Description de mon évènement</label>
+							<textarea name="desc" placeholder="Décrivez votre évènement ici" required><?php echo $resultat[2] ?></textarea>
+							<input name="submitBtn" type="submit" value="Modifier">
+							<a href="reserved.php?id=<?php echo $resultat[0] ?>"><img src="Images/closeBtn.png"/></a>
+						</form>
+					</div>
+				
+				<?php
+				
+				if(isset($_POST["submitBtn"])) {
+					if($_POST["titre"] != $resultat[1]) {
+						sql_request("UPDATE reservations SET titre = '".$_POST["titre"]."' WHERE id = '".$resultat[0]."' ");
+					}
+					if($_POST["desc"] != $resultat[2]) {
+						sql_request("UPDATE reservations SET description = '".$_POST["desc"]."' WHERE id = '".$resultat[0]."' ");
+					}
+					header("location:reserved.php?id=".$resultat[0]);
+				}
+			}
+		}
+	?>
+	
     <?php include("footer.php"); ?>
+
 </body>
 
 </html>
+
