11cabedeee091ca329263d4cc5c26190301e8cc4
[enzo.mandine] <[enzo.mandine@laplateforme.io]>
2020-01-09 15:24:04 +0100

    Clean DB

--- reservationsalles.sql
+++ reservationsalles.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 08 jan. 2020 à 17:49
+-- Généré le :  jeu. 09 jan. 2020 à 14:23
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -39,19 +39,7 @@ CREATE TABLE IF NOT EXISTS `reservations` (
   `fin` datetime NOT NULL,
   `id_utilisateur` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `reservations`
---
-
-INSERT INTO `reservations` (`id`, `titre`, `description`, `debut`, `fin`, `id_utilisateur`) VALUES
-(1, 'test res 1', 'res 1\r\nCoactique aliquotiens nostri pedites ad eos persequendos scandere clivos sublimes etiam si lapsantibus plantis fruticeta prensando vel dumos ad vertices venerint summos, inter arta tamen et invia nullas acies explicare permissi nec firmare nisu valido gressus: hoste discursatore rupium abscisa volvente, ruinis ponderum inmanium consternuntur, aut ex necessitate ultima fortiter dimicante, superati periculose per prona discedunt.', '2020-01-07 08:00:00', '2020-01-07 09:00:00', 1),
-(2, 'test res 2', 'res 2', '2020-01-08 08:00:00', '2020-01-08 09:00:00', 1),
-(3, 'test res 3', 'res 3', '2020-01-09 08:00:00', '2020-01-09 09:00:00', 1),
-(5, 'zerfghjkl', 'gfhg', '2020-01-10 11:00:00', '2020-01-10 12:00:00', 2),
-(6, 'kkkk', 'gfdtfhgvj', '2020-01-07 10:48:00', '2020-01-07 11:00:00', 2),
-(9, 'caca', 'caca', '2020-01-10 08:00:00', '2020-01-10 09:00:00', 10);
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -66,19 +54,14 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `password` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL COMMENT 'name = id.png',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `utilisateurs`
 --
 
 INSERT INTO `utilisateurs` (`id`, `login`, `password`, `image`) VALUES
-(1, 'admin', 'admin', ''),
-(2, 'Enzo', '$2y$10$T7RHDI3nytLklPANiNe8D.R/VaraMOr.mIKW8HJzJCDoM4TOAbvku', ''),
-(5, 'eti', '$2y$10$RMWyoU9JDo.5ybp/vTUpGusDYuWsrL5G1H4l0cFx0Ixy7nfMefgtC', ''),
-(6, 'avatar', '$2y$10$hYrOgEpGTffbhEYp5qONgOFhXFN7P.BJpOznicGhjzEsnFwTVmACa', ''),
-(7, 'marceau', '$2y$10$HHxX/rS5nMLqiFDPHMxbOOpU0Rn.GevQkcQhxG4jxsoCRDOIMUK9e', '0.png'),
-(10, 'caca', '$2y$10$V.Ob0XQ4ojuUt5Qe2H.MXOujdqxT2Og.sJJDIgjOislcOGF9mj5ca', 'ProfilPics/10.png');
+(12, 'Admin', '$2y$10$6sJfFU0Y7SYI.Hgi8erine8ydOn/cqPRohBtTl3LgwM1bwlrVDAz2', '0.png');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
